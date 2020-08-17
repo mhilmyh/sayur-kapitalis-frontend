f1796a9bc4cccd325301a4bf6b3e9f9cbcc6cf7d
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-08-18 06:59:30 +0700

    add alert reset

--- components/alert/FlexibleAlert.js
+++ components/alert/FlexibleAlert.js
@@ -1,10 +1,18 @@
 import Alert from "@material-ui/lab/Alert";
+import { useDispatch } from "react-redux";
+import { alertReset } from "../../redux/actions/creator/alert";
 
 const FlexibleAlert = ({ show = false, error = false, message = "" }) => {
+	const dispatch = useDispatch();
 	if (show) {
 		return (
 			<div className="my-2 w-full">
-				<Alert severity={error ? "error" : "success"}>{message}</Alert>
+				<Alert
+					severity={error ? "error" : "success"}
+					onClose={() => dispatch(alertReset())}
+				>
+					{message}
+				</Alert>
 			</div>
 		);
 	}
