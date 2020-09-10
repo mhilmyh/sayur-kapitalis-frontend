e7d09c7866a497d683a0933994678dd624cb95e0
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-09-10 18:32:58 +0700

    remove logger

--- redux/store.js
+++ redux/store.js
@@ -2,51 +2,46 @@ import hardSet from "redux-persist/lib/stateReconciler/hardSet";
 import { persistStore, persistReducer, createMigrate } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import logger from "redux-logger";
 import thunk from "redux-thunk";
 import reducer from "./reducers";
 
 const migrations = {
-	0: (state) => {
-		// migration clear out x state
-		return {
-			...state,
-		};
-	},
-	1: (state) => {
-		// migration to keep only x state
-		return {
-			x: state.x,
-		};
-	},
+  0: (state) => {
+    // migration clear out x state
+    return {
+      ...state,
+    };
+  },
+  1: (state) => {
+    // migration to keep only x state
+    return {
+      x: state.x,
+    };
+  },
 };
 
 const persistConfig = {
-	key: "X-APP-STATE",
-	storage,
-	version: 0,
-	blacklist: [
-		"loading",
-		"alert",
-		"coverageArea",
-		"informations",
-		"headings",
-		"customers",
-		"provinces",
-		"cities",
-		"districts",
-	],
-	migrate: createMigrate(migrations, { debug: false }),
-	stateReconciler: hardSet,
+  key: "X-APP-STATE",
+  storage,
+  version: 0,
+  blacklist: [
+    "loading",
+    "alert",
+    "coverageArea",
+    "informations",
+    "headings",
+    "customers",
+    "provinces",
+    "cities",
+    "districts",
+  ],
+  migrate: createMigrate(migrations, { debug: false }),
+  stateReconciler: hardSet,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-const store = createStore(
-	persistedReducer,
-	composeWithDevTools(applyMiddleware(thunk, logger))
-);
+const store = createStore(persistedReducer, applyMiddleware(thunk));
 
 export const persistor = persistStore(store);
 
