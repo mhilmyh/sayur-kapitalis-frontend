ac54a7404bdf6d9ecd31c4484c41758aaf881fe9
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-09-10 14:40:23 +0700

    fix indentation

--- components/table/DetailTable.js
+++ components/table/DetailTable.js
@@ -4,133 +4,133 @@ import EditPaymentDialog from "../dialog/EditPaymentDialog";
 import DetailSection from "../section/DetailSection";
 
 const DetailTable = ({ data }) => {
-	const [openOrder, setOpenOrder] = React.useState(false);
-	const [openEditPayment, setOpenEditPayment] = React.useState(false);
-	return (
-		<React.Fragment>
-			<OrderDialog
-				order={data}
-				open={openOrder}
-				onClose={() => setOpenOrder(false)}
-			></OrderDialog>
-			<EditPaymentDialog
-				payment={data.order_payment}
-				open={openEditPayment}
-				onClose={() => setOpenEditPayment(false)}
-			></EditPaymentDialog>
-			<div className="w-full flex flex-wrap justify-center items-center bg-gray-700">
-				<div className="w-full flex bg-gray-100 p-2 mb-1 mt-4 mx-2 justify-between items-center rounded">
-					<div className="font-bold text-green-500 text-xs uppercase">
-						Tanggal Pembuatan
-					</div>
-					<div className="font-semibold text-gray-600 text-xs uppercase">
-						{stringToDate(data.created_at)}
-					</div>
-				</div>
-				<div className="w-full flex bg-gray-100 p-2 mt-1 mx-2 justify-between items-center rounded-t">
-					<div className="font-bold text-green-500 text-xs uppercase">
-						Detail Produk
-					</div>
-					<div className="font-bold bg-green-500 w-2 h-2 rounded-full"></div>
-				</div>
-				<div className="w-full bg-gray-600 p-2 mx-2 rounded-b">
-					<div className="flex justify-between items-center border-b-2 border-gray-500 my-1 py-1">
-						<div className="flex justify-between w-7/8 w-full">
-							<div className="w-1/4 font-semibold text-gray-200 text-xs text-left">
-								Nama Produk
-							</div>
-							<div className="w-1/4 font-semibold text-gray-200 text-xs text-center">
-								Harga
-							</div>
-							<div className="w-1/4 font-semibold text-gray-200 text-xs text-center">
-								Pembeli
-							</div>
-						</div>
-						<div className="w-1/8 text-right">
-							<div className="font-bold bg-gray-500 w-2 h-2 rounded-full float-right"></div>
-						</div>
-					</div>
-					{data.order_details.map((order, index) => (
-						<div
-							key={index}
-							className="flex justify-between items-center border-b-2 border-gray-500 my-1 py-1"
-						>
-							<div className="flex justify-between w-7/8 w-full">
-								<div className="w-1/3 font-semibold text-gray-200 text-xs text-left">
-									{order.product_snapshot.product_name}
-								</div>
-								<div className="w-1/3 font-semibold text-gray-200 text-xs text-center">
-									{convertToRupiah(order.price)}
-								</div>
-								<div className="w-1/3 font-semibold text-gray-200 text-xs text-center">
-									{order.user_full_name}
-								</div>
-							</div>
-							<div className="w-1/8 text-right">
-								<div className="font-bold bg-gray-500 w-2 h-2 rounded-full float-right"></div>
-							</div>
-						</div>
-					))}
-				</div>
-				{!!data.order_payment ? (
-					<React.Fragment>
-						<DetailSection
-							title="Pengiriman"
-							data={[
-								{ name: "Tanggal Pengiriman", value: data.shipment_date },
-								{
-									name: "Waktu Pengiriman",
-									value: `(${data.shipment_time.name}) ${data.shipment_time.start_time} - ${data.shipment_time.end_time}`,
-								},
-							]}
-						></DetailSection>
-						<DetailSection
-							title="Bukti Pembayaran"
-							data={[
-								{
-									name: "Berkas yang diunggah",
-									label: "Link Gambar",
-									url: data.order_payment.image_url,
-								},
-							]}
-						></DetailSection>
-						<div className="w-full flex bg-gray-100 p-2 my-3 mx-2 justify-center items-center rounded">
-							<button
-								id={`buttonPay-${data.id}`}
-								className="px-12 py-2 bg-green-500 text-gray-100 rounded shadow-md"
-								onClick={() => setOpenEditPayment(true)}
-							>
-								Ganti bukti
-							</button>
-						</div>
-						<div className="w-full mb-6"></div>
-					</React.Fragment>
-				) : (
-					<div className="w-full flex bg-gray-100 p-2 my-3 mx-2 justify-between items-center rounded">
-						<a
-							className="font-bold text-green-500 text-xs uppercase flex justify-between items-center"
-							href={`#buttonPay-${data.id}`}
-						>
-							<span>Bayar Sekarang</span>
-							<svg className="svg-icon" viewBox="0 0 20 20">
-								<path
-									fill="none"
-									d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z"
-								></path>
-							</svg>
-						</a>
-						<button
-							id={`buttonPay-${data.id}`}
-							className="px-6 py-2 bg-green-500 text-gray-100 rounded shadow-md"
-							onClick={() => setOpenOrder(true)}
-						>
-							Bayar
-						</button>
-					</div>
-				)}
-			</div>
-		</React.Fragment>
-	);
+  const [openOrder, setOpenOrder] = React.useState(false);
+  const [openEditPayment, setOpenEditPayment] = React.useState(false);
+  return (
+    <React.Fragment>
+      <OrderDialog
+        order={data}
+        open={openOrder}
+        onClose={() => setOpenOrder(false)}
+      ></OrderDialog>
+      <EditPaymentDialog
+        payment={data.order_payment}
+        open={openEditPayment}
+        onClose={() => setOpenEditPayment(false)}
+      ></EditPaymentDialog>
+      <div className="w-full flex flex-wrap justify-center items-center bg-gray-700">
+        <div className="w-full flex bg-gray-100 p-2 mb-1 mt-4 mx-2 justify-between items-center rounded">
+          <div className="font-bold text-green-500 text-xs uppercase">
+            Tanggal Pembuatan
+          </div>
+          <div className="font-semibold text-gray-600 text-xs uppercase">
+            {stringToDate(data.created_at)}
+          </div>
+        </div>
+        <div className="w-full flex bg-gray-100 p-2 mt-1 mx-2 justify-between items-center rounded-t">
+          <div className="font-bold text-green-500 text-xs uppercase">
+            Detail Produk
+          </div>
+          <div className="font-bold bg-green-500 w-2 h-2 rounded-full"></div>
+        </div>
+        <div className="w-full bg-gray-600 p-2 mx-2 rounded-b">
+          <div className="flex justify-between items-center border-b-2 border-gray-500 my-1 py-1">
+            <div className="flex justify-between w-7/8 w-full px-4">
+              <div className="w-1/4 font-semibold text-gray-200 text-xs text-left">
+                Nama Produk
+              </div>
+              <div className="w-1/4 font-semibold text-gray-200 text-xs text-center">
+                Harga
+              </div>
+              <div className="w-1/4 font-semibold text-gray-200 text-xs text-right">
+                Pembeli
+              </div>
+            </div>
+            <div className="w-1/8 text-right">
+              <div className="font-bold bg-gray-500 w-2 h-2 rounded-full float-right"></div>
+            </div>
+          </div>
+          {data.order_details.map((order, index) => (
+            <div
+              key={index}
+              className="flex justify-between items-center border-b-2 border-gray-500 my-1 py-1"
+            >
+              <div className="flex justify-between w-7/8 w-full px-4">
+                <div className="w-1/3 font-semibold text-gray-200 text-xs text-left">
+                  {order.product_snapshot.product_name}
+                </div>
+                <div className="w-1/3 font-semibold text-gray-200 text-xs text-center">
+                  {convertToRupiah(order.price)}
+                </div>
+                <div className="w-1/3 font-semibold text-gray-200 text-xs text-right">
+                  {order.user_full_name}
+                </div>
+              </div>
+              <div className="w-1/8 text-right">
+                <div className="font-bold bg-gray-500 w-2 h-2 rounded-full float-right"></div>
+              </div>
+            </div>
+          ))}
+        </div>
+        {!!data.order_payment ? (
+          <React.Fragment>
+            <DetailSection
+              title="Pengiriman"
+              data={[
+                { name: "Tanggal Pengiriman", value: data.shipment_date },
+                {
+                  name: "Waktu Pengiriman",
+                  value: `(${data.shipment_time.name}) ${data.shipment_time.start_time} - ${data.shipment_time.end_time}`,
+                },
+              ]}
+            ></DetailSection>
+            <DetailSection
+              title="Bukti Pembayaran"
+              data={[
+                {
+                  name: "Berkas yang diunggah",
+                  label: "Link Gambar",
+                  url: data.order_payment.image_url,
+                },
+              ]}
+            ></DetailSection>
+            <div className="w-full flex bg-gray-100 p-2 my-3 mx-2 justify-center items-center rounded">
+              <button
+                id={`buttonPay-${data.id}`}
+                className="px-12 py-2 bg-green-500 text-gray-100 rounded shadow-md"
+                onClick={() => setOpenEditPayment(true)}
+              >
+                Ganti bukti
+              </button>
+            </div>
+            <div className="w-full mb-6"></div>
+          </React.Fragment>
+        ) : (
+          <div className="w-full flex bg-gray-100 p-2 my-3 mx-2 justify-between items-center rounded">
+            <a
+              className="font-bold text-green-500 text-xs uppercase flex justify-between items-center"
+              href={`#buttonPay-${data.id}`}
+            >
+              <span>Bayar Sekarang</span>
+              <svg className="svg-icon" viewBox="0 0 20 20">
+                <path
+                  fill="none"
+                  d="M11.611,10.049l-4.76-4.873c-0.303-0.31-0.297-0.804,0.012-1.105c0.309-0.304,0.803-0.293,1.105,0.012l5.306,5.433c0.304,0.31,0.296,0.805-0.012,1.105L7.83,15.928c-0.152,0.148-0.35,0.223-0.547,0.223c-0.203,0-0.406-0.08-0.559-0.236c-0.303-0.309-0.295-0.803,0.012-1.104L11.611,10.049z"
+                ></path>
+              </svg>
+            </a>
+            <button
+              id={`buttonPay-${data.id}`}
+              className="px-6 py-2 bg-green-500 text-gray-100 rounded shadow-md"
+              onClick={() => setOpenOrder(true)}
+            >
+              Bayar
+            </button>
+          </div>
+        )}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default React.memo(DetailTable);
