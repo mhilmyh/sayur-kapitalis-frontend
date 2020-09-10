c3d1b0aa4ec7b0d4374f372e13796d23304af31a
Muhamad Hilmy Haidar <hilmy_21@apps.ipb.ac.id>
2020-09-10 17:32:55 +0700

    add link

--- components/section/HeadingSection.js
+++ components/section/HeadingSection.js
@@ -1,37 +1,40 @@
 import Carousel from "react-material-ui-carousel";
 import { useDispatch, useSelector } from "react-redux";
 import { headingFetch } from "../../redux/actions/creator/information";
+import Link from "next/link";
 
 const Item = ({ item = {} }) => {
-	return (
-		<div className="w-full relative">
-			<img
-				src={item.image_url}
-				className="w-full bg-contain object-contain rounded h-64"
-			></img>
-			{/* <h2 className="w-full absolute bottom-0 mb-8 flex justify-center">
+  return (
+    <Link href="/info">
+      <div className="w-full relative cursor-pointer">
+        <img
+          src={item.image_url}
+          className="w-full bg-contain object-contain rounded h-64"
+        ></img>
+        {/* <h2 className="w-full absolute bottom-0 mb-8 flex justify-center">
 				<span className="bg-green-500 text-white font-semibold rounded p-4 shadow-lg">
 					{item.title}
 				</span>
 			</h2> */}
-		</div>
-	);
+      </div>
+    </Link>
+  );
 };
 
 const HeadingSection = () => {
-	const dispatch = useDispatch();
-	const headings = useSelector((state) => state.headings);
-	React.useEffect(() => {
-		dispatch(headingFetch());
-	}, []);
-	return (
-		<div className="w-full flex justify-center items-center mb-4 h-100">
-			<Carousel navButtonsAlwaysVisible={true} className="w-full">
-				{!!headings &&
-					headings.map((item, index) => <Item key={index} item={item}></Item>)}
-			</Carousel>
-		</div>
-	);
+  const dispatch = useDispatch();
+  const headings = useSelector((state) => state.headings);
+  React.useEffect(() => {
+    dispatch(headingFetch());
+  }, []);
+  return (
+    <div className="w-full flex justify-center items-center mb-4 h-100">
+      <Carousel navButtonsAlwaysVisible={true} className="w-full">
+        {!!headings &&
+          headings.map((item, index) => <Item key={index} item={item}></Item>)}
+      </Carousel>
+    </div>
+  );
 };
 
 export default React.memo(HeadingSection);
